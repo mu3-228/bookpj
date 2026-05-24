85ee0efa91cf6452d82bb454352012fe125233cd
mu3-228 <mu-_-@ymail.ne.jp>
2026-05-25 00:05:14 +0900

    first commit

--- book/views.py
+++ book/views.py
@@ -2,29 +2,30 @@ from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, DetailView,CreateView,DeleteView,UpdateView
 from .models import Book, Review
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 
-class ListBookView(ListView):
+class ListBookView(LoginRequiredMixin, ListView):
     template_name = 'book/book_list.html'
     model = Book
-class DetailBookView(DetailView):
+class DetailBookView(LoginRequiredMixin, DetailView):
     model = Book
-class CreateBookView(CreateView):
+class CreateBookView(LoginRequiredMixin, CreateView):
     template_name = 'book/book_create.html'
     model = Book
     fields = ('title', 'text', 'category', 'thumbnail')
     success_url = reverse_lazy('list-book')
-class DeleteBookView(DeleteView):
+class DeleteBookView(LoginRequiredMixin, DeleteView):
     template_name = 'book/book_confirm_delete.html'
     model = Book
     success_url = reverse_lazy('list-book')
-class UpdateBookView(UpdateView):
+class UpdateBookView(LoginRequiredMixin, UpdateView):
     template_name = 'book/book_update.html'
     model = Book
     fields = ('title', 'text', 'category', 'thumbnail')
     success_url = reverse_lazy('list-book')
-class CreateReviewView(CreateView):
+class CreateReviewView(LoginRequiredMixin, CreateView):
     model = Review
     fields = ('title', 'text', 'rate')
     template_name = 'book/review_form.html'
@@ -36,8 +37,8 @@ class CreateReviewView(CreateView):
         print(context['form'].errors)  # ←これ追加
         return context
     
-    def form_valid(seif, form):
-        form.instrnce.user = seif.request.user
+    def form_valid(self, form):
+        form.instance.user = self.request.user
 
 
         return super().form_valid(form)
@@ -48,6 +49,7 @@ class CreateReviewView(CreateView):
         form.instance.book = Book.objects.get(pk=self.kwargs['book_id'])
         form.instance.user = self.request.user
         return super().form_valid(form)
+    #   ↓レビュー対象の本を設定したり、レビュー後どこにいくか
     def get_success_url(self):
         return reverse_lazy('detail-book', kwargs={'pk': self.kwargs['book_id']})
     
