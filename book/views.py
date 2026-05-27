4f819989f7d6ad7601ce2300e4be0dd9e4577ea1
mu3-228 <mu-_-@ymail.ne.jp>
2026-05-27 23:35:23 +0900

    first commit

--- book/views.py
+++ book/views.py
@@ -43,7 +43,7 @@ class CreateReviewView(LoginRequiredMixin, CreateView):
 
         return super().form_valid(form)
     #
-    def form_valid(self, form):
+    #def form_valid(self, form):
         print("🔥通った🔥")
         print(form.errors)
         form.instance.book = Book.objects.get(pk=self.kwargs['book_id'])
